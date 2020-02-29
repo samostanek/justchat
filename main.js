2788bc4c057af78b26eeff005b04b4a1e236c34e
samostanek <samostanek@gmail.com>
2020-02-29 18:30:35 +0100

    Added message sending
    - Added nessage sending
    - User can now choose Port and his Name

--- main.js
+++ main.js
@@ -1,20 +1,51 @@
-const express = require('express');
-const readline = require('readline');
-const { pingAll } = require('./misc.js');
-const port = 40204;
+const express = require("express");
+const bodyParser = require("body-parser");
+const readline = require("readline");
+const cliProgress = require("cli-progress");
+const { pingAll, sendMessage } = require("./misc.js");
 var app = express();
+var port = undefined;
+var name = "undefined";
+var members = [];
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-rl.on('line', input => {
-    if (input.split(' ')[0] == 'ping') pingAll(input.split(' ')[1]).then(ips => console.log("Found IPs: " + ips));
+rl.on("line", input => {
+    if (input.split(" ")[0] == ">") {
+        if (input.split(" ")[1] == "ping") {
+            pingAll(input.split(" ")[1], undefined).then(ips => console.log("Found IPs: " + ips));
+        }
+    } else sendMessage(input, members, port, name);
 });
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/ping', (req, res) => res.json({ online: true }));
+app.use(bodyParser.json({ extended: true }));
 
-app.listen(port, () => console.log(`App is listening on port ${port}`));
+app.get("/ping", (req, res) => res.json({ online: true, name: name }));
+
+app.post("/msgIn", (req, res) => {
+    console.log(req.body.name + ' -> all: ' + req.body.msg);
+    res.sendStatus(200);
+});
+
+rl.question("Listen port: ", port_ => {
+    rl.question("Your name: ", name_ => {
+        name = name_;
+        port = port_;
+        app.listen(port, () => {
+            console.log(`App is listening on port ${port}`);
+            const pingBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+            console.log("Searching members: ");
+            pingBar.start(256);
+            pingAll(port, pingBar).then(ips => {
+                pingBar.stop();
+                members = members.concat(ips);
+                console.log("Found IPs: " + ips);
+            });
+        });
+    });
+});
--- misc.js
+++ misc.js
@@ -12,11 +12,10 @@ function ping(options, callback) {
 
         res.on("end", () => {
             try {
-                callback(JSON.parse(output), undefined);
+                callback([JSON.parse(output), options.hostname], undefined);
             } catch (error) {
                 callback(undefined, error);
             }
-            
         });
     })
         .on("error", error => {
@@ -25,21 +24,42 @@ function ping(options, callback) {
         .end();
 }
 
-function pingAll(port) {
+function pingAll(port, bar) {
     return new Promise((res, rej) => {
         var found = [];
         var done = 0;
         for (i = 0; i < 256; i++) {
             myIP = ip.address().split(".");
             pingIP = myIP[0] + "." + myIP[1] + "." + myIP[2] + "." + i;
-            ping({ hostname: pingIP, port: port, path: "/", method: "GET" }, (out, err) => {
+            ping({ hostname: pingIP, port: port, path: "/ping", method: "GET" }, (out, err) => {
                 done++;
-                console.log(done);
-                if (!err) found.push(pingIP);
+                if (bar) bar.update(done);
+                if (!err) if (out[0].online) found.push([out[1], out[0].name]);
                 if (done >= 256) res(found);
             });
         }
     });
 }
 
+function sendMessage(msg, members, port, name) {
+    for (let m of members) {
+        const req = http.request(
+            {
+                hostname: m[0],
+                port: port,
+                path: "/msgIn",
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            },
+            res => {}
+        );
+        req.on("error", error => console.error(error));
+        req.write(JSON.stringify({ msg: msg, name: name }));
+        req.end();
+    }
+}
+
 module.exports.pingAll = pingAll;
+module.exports.sendMessage = sendMessage;
